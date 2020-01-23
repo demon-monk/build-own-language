09b37aca88a4c2ad121ed1312f555e42b70a5bdc
demon-monk <antdude@126.com>
2020-01-23 16:37:16 +0800

    finish the evaluate part

---
+++ docs/repl.md
@@ -0,0 +1,3 @@
+## Intro
+
+**Read Evaluate Print Loop**
--- src/standard-library.js
+++ src/standard-library.js
@@ -4,9 +4,9 @@ const add = all((a, b) => a + b);
 const subtract = all((a, b) => a - b);
 const multiply = all((a, b) => a * b);
 const divide = all((a, b) => a / b);
+const max = all((a, b) => Math.max(a, b));
 const modulo = all((a, b) => a % b);
 const log = console.log;
-
 const environment = {
   add,
   subtract,
@@ -14,6 +14,7 @@ const environment = {
   divide,
   modulo,
   log,
+  max,
   pi: Math.PI,
 };
 
