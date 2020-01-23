0ce4607d48ebf5d4315480c8b39339b12fd7df29
demon-monk <antdude@126.com>
2020-01-24 00:17:07 +0800

    finish traverse part

---
+++ docs/generation.md
@@ -0,0 +1,9 @@
+## visitor pattern
+
+- allows you to add methods to classes of different types without much altering to those classes
+- you can make completely different methods depending on the class used
+- allows you to define external classes that can extend other class without majorly editing them
+
+### visitor pattern
+
+![](http://www.plantuml.com/plantuml/svg/SoWkIImgAStDuShCAqajIajCJbK8oopEBCalKgZcKW22CX1FmoUpi3I_IDDA8IM_D2ad5Kac93yfCJav7vkKNsfoQZ6GKOcGhENYIiv9B2vM24cieB4zI3RebHRerTZ1FCQ5xjT4e0weAu1Mf1QKQ41LOd4tt2w26k0gKQnF14bo8QpLrKMuoo4rBmKO5G00)
--- src/traverse.js
+++ src/traverse.js
@@ -1,3 +1,22 @@
-const traverse = (node, visitor) => {};
+const traverseArray = ({ array, parent, visitor }) => {
+  array.forEach(node => {
+    traverseNode({ node, parent, visitor });
+  });
+};
+const traverseNode = ({ node, parent, visitor }) => {
+  const methods = visitor[node.type];
+  if (methods && methods.enter) {
+    methods.enter({ node, parent });
+  }
+  if (node.arguments) {
+    traverseArray({ array: node.arguments, parent: node, visitor });
+  }
+  if (methods && methods.exit) {
+    methods.exit({ node, parent });
+  }
+};
+const traverse = (node, visitor) => {
+  traverseNode({ node, visitor });
+};
 
 module.exports = { traverse };
